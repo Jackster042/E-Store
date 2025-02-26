1b4af4a07ab95173d7d47634e78232b9483c9640
Nemanja Stojanovic <feemail042@gmail.com>
2025-02-27 00:32:02 +0100

    Building the CartSection - Cart Backend part done

--- server/controllers/shop/cartController.js
+++ server/controllers/shop/cartController.js
@@ -1,6 +1,50 @@
+const ProductModel = require("../../models/ProductModels");
+const CartModel = require("../../models/CartModel");
+const UserModel = require("../../models/UserModel");
+
 exports.addToCart = async (req, res, next) => {
   try {
-    res.send("Hello from addToCart - BACKEND");
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || !quantity) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields",
+      });
+    }
+
+    const product = await ProductModel.findById(productId);
+    if (!product)
+      return res
+        .status(400)
+        .json({ success: false, message: "Product not found!" });
+
+    const cart = await CartModel.findOne({ userId });
+
+    if (!cart) {
+      cart = new CartModel({
+        userId,
+        items: [],
+      });
+    }
+
+    const existingItemIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (existingItemIndex === -1) {
+      cart.items.push({ productId, quantity });
+    } else {
+      cart.items[existingItemIndex].quantity = +quantity;
+    }
+
+    await cart.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Item added to cart successfully",
+      data: cart,
+    });
   } catch (error) {
     console.error(error, "error from addToCart - BACKEND");
     res.status(500).json({
@@ -12,6 +56,56 @@ exports.addToCart = async (req, res, next) => {
 
 exports.getCart = async (req, res, next) => {
   try {
+    const { userId } = req.body;
+
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        message: "User ID is required",
+      });
+    }
+
+    const cart = await CartModel.findOne({ userId }).populate({
+      path: "items.productId",
+      select: "image title price",
+    });
+
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found",
+      });
+    }
+
+    // CHECK IF THE PRODUCT IS IN STOCK
+    const validateItems = cart.items.filter(
+      (productItem) => productItem.productId
+    );
+    console.log(validateItems, "validateItems");
+
+    if (validateItems.length < cart.items.length) {
+      cart.items = validateItems;
+      await cart.save();
+    }
+
+    const populateCartItems = validateItems.map((item) => ({
+      productId: item.productId._id,
+      image: item.productId.image,
+      title: item.productId.title,
+      price: item.productId.price,
+      quantity: item.quantity,
+      salePrice: item.productId.salePrice,
+    }));
+
+    return res.status(200).json({
+      success: true,
+      message: "Cart fetched successfully",
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
+
     res.send("Hello from getCart - BACKEND");
   } catch (error) {
     console.error(error, "error from getCart - BACKEND");
@@ -24,7 +118,61 @@ exports.getCart = async (req, res, next) => {
 
 exports.updateQuantity = async (req, res, next) => {
   try {
-    res.send("Hello from updateQuantity - BACKEND");
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || !quantity) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields",
+      });
+    }
+
+    const cart = await CartModel.findOne({ userId });
+
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found",
+      });
+    }
+
+    const existingItemIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (existingItemIndex === -1) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found in cart",
+      });
+    }
+
+    cart.items[existingItemIndex].quantity = quantity;
+
+    await cart.save();
+
+    await cart.populate({
+      path: "items.productId",
+      select: "image title price",
+    });
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      image: item.productId ? item.productId.image : null,
+      title: item.productId ? item.productId.title : "Product not found",
+      price: item.productId ? item.productId.price : null,
+      salePrice: item.productId ? item.productId.salePrice : null,
+      quantity: item.quantity,
+    }));
+
+    return res.status(200).json({
+      success: true,
+      message: "Quantity updated successfully",
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
   } catch (error) {
     console.error(error, "error from updateQuantity - BACKEND");
     res.status(500).json({
@@ -36,6 +184,45 @@ exports.updateQuantity = async (req, res, next) => {
 
 exports.removeFromCart = async (req, res, next) => {
   try {
+    const { userId, productId } = req.body;
+    if (!userId || !productId) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields",
+      });
+    }
+
+    const cart = await CartModel.findOne({ userId });
+    if (!cart)
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found",
+      });
+
+    // FILTER THE ITEMS THAT ARE NOT THE PRODUCT TO BE REMOVED
+    cart.items = cart.items.filter(
+      (item) => item.productId.toString() !== productId
+    );
+    await cart.save();
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      image: item.productId ? item.productId.image : null,
+      title: item.productId ? item.productId.title : "Product not found",
+      price: item.productId ? item.productId.price : null,
+      salePrice: item.productId ? item.productId.salePrice : null,
+      quantity: item.quantity,
+    }));
+
+    return res.status(200).json({
+      success: true,
+      message: "Product removed from cart successfully",
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
+
     res.send("Hello from removeFromCart - BACKEND");
   } catch (error) {
     console.error(error, "error from removeFromCart - BACKEND");
--- server/routes/cart/cartRoutes.js
+++ server/routes/cart/cartRoutes.js
@@ -8,6 +8,9 @@ const {
   removeFromCart,
 } = require("../../controllers/shop/cartController");
 
+router.get("/get/:userId", getCart);
 router.post("/add", addToCart);
+router.put("/update-cart", updateQuantity);
+router.delete("/:userId/:productId", removeFromCart);
 
 module.exports = router;
