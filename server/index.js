283c33512be618c8816c185f6c43bd8b30efe857
Nemanja Stojanovic <feemail042@gmail.com>
2025-07-31 14:01:48 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -21,22 +21,16 @@ const featureRoutes = require("./routes/common/featureRoutes");
 // MIDDLEWARES
 app.use(
     cors({
-        origin: function (origin, callback) {
-            const allowedOrigins = [
-                "https://e-store-client.onrender.com",
-                "http://localhost:5173"
-            ];
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
+        origin: [
+            "https://e-store-client.onrender.com",
+            "http://localhost:5173"
+        ],
         credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Added OPTIONS
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allowedHeaders: [
             "Content-Type",
             "Authorization",
+            "Cache-Control",  // Explicitly allow Cache-Control
             "X-Requested-With",
             "Accept"
         ],
@@ -44,7 +38,7 @@ app.use(
     })
 );
 
-// Handle preflight requests
+// Explicitly handle OPTIONS requests
 app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
