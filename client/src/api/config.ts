7fc9f15c2056634d1abb2692249e23b4301ca736
Nemanja Stojanovic <feemail042@gmail.com>
2025-08-01 17:53:23 +0200

    api

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "https://e-store-q3b8.onrender.com";
 
 // Validate API URL
 if (!API_URL) {
